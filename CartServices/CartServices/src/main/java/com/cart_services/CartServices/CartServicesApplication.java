42c9827265072af634ed7e2afd09b1af413fbda6
Priyanshu <pv254424@gmail.com>
2026-03-28 00:00:36 +0530

    adding new microservices

--- CartServices/CartServices/src/main/java/com/cart_services/CartServices/CartServicesApplication.java
+++ CartServices/CartServices/src/main/java/com/cart_services/CartServices/CartServicesApplication.java
@@ -1,4 +1,4 @@
-package com.product_service.ProductService;
+package com.cart_services.CartServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProductServiceApplication {
+public class CartServicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+		SpringApplication.run(CartServicesApplication.class, args);
 	}
 
 }
---
+++ CartServices/CartServices/src/test/java/com/cart_services/CartServices/CartServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cart_services.CartServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
