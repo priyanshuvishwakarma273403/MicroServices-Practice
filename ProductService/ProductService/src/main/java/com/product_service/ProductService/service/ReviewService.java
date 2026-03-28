89fdd4e7849e72d92cb9b90221257dd2a2c38261
Priyanshu <pv254424@gmail.com>
2026-03-28 11:55:07 +0530

    frontent add

--- ProductService/ProductService/src/main/java/com/product_service/ProductService/service/ReviewService.java
+++ ProductService/ProductService/src/main/java/com/product_service/ProductService/service/ReviewService.java
@@ -1,5 +1,14 @@
 package com.product_service.ProductService.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+
+    private final 
 
 }
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-green-600">
+      Tailwind v4 Working ✅
+    </h1>
+  )
+}
+export default App
