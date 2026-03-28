4ef7177d9f3c3cb1e446405ee8644078173516b5
Priyanshu <pv254424@gmail.com>
2026-03-28 11:25:43 +0530

    model service done

--- ProductService/ProductService/src/main/java/com/product_service/ProductService/model/Review.java
+++ ProductService/ProductService/src/main/java/com/product_service/ProductService/model/Review.java
@@ -1,6 +1,43 @@
 package com.product_service.ProductService.model;
 
-import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+
+@Document(collection = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Review implements Serializable {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String productId;
+
+    private Long userId;
+
+    private String userName;
+
+    @Builder.Default
+    private Integer rating = 5;    // 1 to 5
+
+    private String title;
+
+    private String comment;
+
+    private Boolean verified; // verified purchase?
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
