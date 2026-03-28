6a1e9200d5dc3d4a0c57461ff862b535155ac1e8
Priyanshu <pv254424@gmail.com>
2026-03-28 10:47:40 +0530

    order service initialize

---
+++ OrderService/OrderService/src/main/java/com/order_service/OrderService/model/Order.java
@@ -0,0 +1,19 @@
+package com.order_service.OrderService.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+
+}
