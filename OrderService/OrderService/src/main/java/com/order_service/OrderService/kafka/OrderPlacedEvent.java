49bf6a4d78cadbf7164b907f92cd0180510f09a3
Priyanshu <pv254424@gmail.com>
2026-03-28 11:03:23 +0530

    order placed event

--- OrderService/OrderService/src/main/java/com/order_service/OrderService/kafka/OrderPlacedEvent.java
+++ OrderService/OrderService/src/main/java/com/order_service/OrderService/kafka/OrderPlacedEvent.java
@@ -1,4 +1,28 @@
 package com.order_service.OrderService.kafka;
 
-public class OrderPlacedEvent {
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderPlacedEvent extends BaseEvent {
+    private Long orderId;
+    private Long userId;
+    private Double totalAmount;
+    private String shippingAddress;
+    private List<OrderItemDto> items;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class OrderItemDto {
+        private String productId;
+        private String productName;
+        private Integer quantity;
+        private Double price;
+    }
 }
---
+++ OrderService/OrderService/src/main/java/com/order_service/OrderService/kafka/PaymentDoneEvent.java
@@ -0,0 +1,5 @@
+package com.order_service.OrderService.kafka;
+
+public class PaymentDoneEvent extends BaseEvent{
+
+}
