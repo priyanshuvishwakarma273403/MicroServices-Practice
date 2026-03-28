730eaf4473beb8db74ef26e3c19f4c0321f22ed7
Priyanshu <pv254424@gmail.com>
2026-03-28 11:05:19 +0530

    order payment service done

---
+++ OrderService/OrderService/src/main/java/com/order_service/OrderService/kafka/OrderConfirmedEvent.java
@@ -0,0 +1,5 @@
+package com.order_service.OrderService.kafka;
+
+public class OrderConfirmedEvent extends BaseEvent {
+
+}
--- OrderService/OrderService/src/main/java/com/order_service/OrderService/kafka/PaymentDoneEvent.java
+++ OrderService/OrderService/src/main/java/com/order_service/OrderService/kafka/PaymentDoneEvent.java
@@ -1,5 +1,17 @@
 package com.order_service.OrderService.kafka;
 
+
+import lombok.*;
+
+@Data
+@EqualsAndHashCode(callSuper=true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PaymentDoneEvent extends BaseEvent{
+    private Long orderId;
+    private String paymentId;
+    private String status;
+    private Double amount;
 
 }
